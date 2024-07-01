a4223b8396c802e9be342f66fb6a346d0dea9116
y <459419708@qq.com>
2024-07-02 00:02:26 +0800

    add cast integer

--- cast/cast_test.go
+++ cast/cast_test.go
@@ -15,3 +15,31 @@ func TestToString(t *testing.T) {
 		t.Error("cast to string failed")
 	}
 }
+
+func TestToInt(t *testing.T) {
+	r := ToInt("-123456")
+	if r != -123456 {
+		t.Error("cast to int failed")
+	}
+}
+
+func TestToUInt(t *testing.T) {
+	r := ToUInt("123456")
+	if r != 123456 {
+		t.Error("cast to uint failed")
+	}
+}
+
+func TestToInt64(t *testing.T) {
+	r := ToInt64("-1444784865584")
+	if r != -1444784865584 {
+		t.Error("cast to int64 failed")
+	}
+}
+
+func TestToUInt64(t *testing.T) {
+	r := ToUInt64("1444784865584")
+	if r != 1444784865584 {
+		t.Error("cast to uint64 failed")
+	}
+}
